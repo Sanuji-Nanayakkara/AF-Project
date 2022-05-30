389957356071695258794321ba81f38c5fee8d7a
Sanuji Nanayakkara <87995724+Sanuji-Nanayakkara@users.noreply.github.com>
2022-05-30 11:02:40 +0530

    commit2

---
+++ frontend/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Index from './components/home/index';
+import StudentReg from './components/student/student_reg';
+import Login from './components/home/login';
+import TeamDashboard from './components/student/teamDashboard';
+import RegDashboard from './components/home/reg_dashboard';
+import StaffTReg from './components/staff/staff_reg';
+import AdminDashboard from './components/admin/admin_dashboard';
+import AdminViewStaff from './components/admin/admin_view_staff';
+import AdminViewOneStaff from './components/admin/staff_one_view';
+import ViewMarkingschemes from './components/admin/admin_view_marking_schemes';
+
+
+
+function App() {
+  return (
+  <div>
+       <Router>
+            <Switch>
+                <Route exact path="/"><Index/></Route>
+                <Route exact path="/student/Reg"><StudentReg/></Route>
+                <Route exact path="/Login"><Login/></Route>
+                <Route exact path="/RegDashboard"><RegDashboard/></Route>
+                <Route exact path="/student/TeamDashboard"><TeamDashboard/></Route>
+                <Route exact path="/staff/StaffTReg"><StaffTReg/></Route>
+                <Route exact path="/admin/AdminDashboard"><AdminDashboard/></Route>
+                <Route exact path="/admin/AdminViewStaff"><AdminViewStaff/></Route>
+                <Route exact path="/admin/AdminViewOneStaff"><AdminViewOneStaff/></Route>
+                <Route exact path="/admin/ViewMarkingschemes"><ViewMarkingschemes/></Route>
+            </Switch>
+      </Router>
+
+  </div>
+    );
+}
+
+export default App;
+
+
