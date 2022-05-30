4c1083a77236ac879289b08fabcb81b6cfac0f0a
GayeshaUpeshika <104220688+GayeshaUpeshika@users.noreply.github.com>
2022-05-30 19:09:35 +0530

    admin_6

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -10,6 +10,10 @@ import AdminDashboard from './components/admin/admin_dashboard';
 import AdminViewStaff from './components/admin/admin_view_staff';
 import AdminViewOneStaff from './components/admin/staff_one_view';
 import ViewMarkingschemes from './components/admin/admin_view_marking_schemes';
+import Staff_one_markingSchema from './components/admin/staff_one_markingSchema';
+import AdminViewDeadLines from './components/admin/admin_view_deadline';
+import OneDeadline from './components/admin/one_deadline';
+import StudentGroupManage from './components/admin/studentGroupManage';
 
 
 
@@ -28,6 +32,10 @@ function App() {
                 <Route exact path="/admin/AdminViewStaff"><AdminViewStaff/></Route>
                 <Route exact path="/admin/AdminViewOneStaff"><AdminViewOneStaff/></Route>
                 <Route exact path="/admin/ViewMarkingschemes"><ViewMarkingschemes/></Route>
+                <Route exact path="/admin/OneMarkingSchemaView"><Staff_one_markingSchema/></Route>
+                <Route exact path="/admin/AdminViewDeadLines"><AdminViewDeadLines/></Route>
+                <Route exact path="/admin/OneDeadline"><OneDeadline/></Route>
+                <Route exact path="/admin/StudentGroupManage"><StudentGroupManage/></Route>
             </Switch>
       </Router>
 
