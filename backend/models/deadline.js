b45127962e4d3d47d005518ad66aab6bb9ee73da
GayeshaUpeshika <104220688+GayeshaUpeshika@users.noreply.github.com>
2022-05-30 19:06:34 +0530

    admin_2

---
+++ backend/models/deadline.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const deadLineRegSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+
+    deadLineDateTime: {
+        type: String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    FileName: {
+        type: String,
+        required: true,
+    },
+
+    status: {
+        type: String,
+        required: true,
+    },
+  
+});
+const deadLine_Schema = mongoose.model('deadLine', deadLineRegSchema);
+module.exports = deadLine_Schema;
